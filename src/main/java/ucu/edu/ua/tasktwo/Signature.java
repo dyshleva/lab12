f5b1fd3b7cb3360e6f0907ce6ff07e093a6bc058
dyshleva <116552489+dyshleva@users.noreply.github.com>
2023-12-10 23:06:13 +0200

    Add files via upload

---
+++ src/main/java/ucu/edu/ua/tasktwo/Signature.java
@@ -0,0 +1,16 @@
+package ucu.edu.ua.tasktwo;
+
+import java.util.function.Consumer;
+
+public class Signature<T> extends Task<T> {
+    private Consumer<T> consumer;
+
+    public Signature(Consumer<T> consumer) {
+        this.consumer = consumer;
+    }
+
+    public void apply(T arg) {
+        this.freeze();
+        consumer.accept(arg);
+    }
+}
